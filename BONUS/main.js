d72761d8c1dcb2797c8b8bfc178ca90e0ef5ba25
zhLisha <lisazhou1709@gmail.com>
2022-06-20 16:48:39 +0200

    changed date format

--- BONUS/main.js
+++ BONUS/main.js
@@ -1,17 +1,11 @@
 /*
-- Creazione Array dei post con i seguenti oggetti:
-    a. Id Post con numero da 1 a n
-    b. Nome Autore
-    c. Foto autore
-    d. Data in formato americano mm-dd-yyyy
-    e. Testo del post
-    f. Immagine (Non tutti i post devono avere un'immagine)
-    g. Numero likes
-- Stampiamo i dati dell'array nell'HTML
-- Al click del tasto mi piace, il colore di quest'ultimo cambia e si incrementano i like relativi. 
+- Formattare la data da americata a locale.
+- Gestire assena immagine profilo con solo le iniziali del'utente
+- Al click Mi piace, se abbiamo gia' cliccato dobbiamo decrementare il contatore e cambiare colore
 */
 
-
+// Data formato italiano
+const currentDate = new Date().toLocaleDateString();
 
 // Creazione Array di tutte le info del post
 const post = [
@@ -19,7 +13,7 @@ const post = [
         id: 1,
         name: 'Sofia Perlari',
         profilePhoto: 'https://i.picsum.photos/id/653/300/300.jpg?hmac=hF6OcvJSGl31yRN1m7iHxShvwbPLYvNv3yPimTdbGDE',
-        publishDate: '06/22/2019',
+        publishDate: currentDate,
         postDescription: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://i.picsum.photos/id/476/300/300.jpg?hmac=tLlHeIdBoPqgpgAQhkNffWra3o59dd-m0UkLxwiL8aM',
         likesCounter: 454
@@ -28,7 +22,7 @@ const post = [
         id: 1,
         name: 'Liam Willstone',
         profilePhoto: 'https://i.picsum.photos/id/28/300/300.jpg?hmac=G2cdhmuBEY2rDoSefRxiQLssBctP0GCKz_UhmEU1JIA',
-        publishDate: '06/22/2019',
+        publishDate: currentDate,
         postDescription: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: null,
         likesCounter: 239
@@ -37,7 +31,7 @@ const post = [
         id: 1,
         name: 'Anya West',
         profilePhoto: 'https://i.picsum.photos/id/300/300/300.jpg?hmac=wrp7T9sii9wxZpkh94Y1rS1UmaSPesyuK2zFta_g8Zo',
-        publishDate: '06/22/2019',
+        publishDate: currentDate,
         postDescription: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
         postImage: 'https://i.picsum.photos/id/9/300/300.jpg?hmac=Zf_elnyFDTPzb9nUe7m1J5g080C689yQsh3U8_DhHWE',
         likesCounter: 683
