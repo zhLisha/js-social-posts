c923864505d205b72e93041f910e3ca6dc299709
zhLisha <lisazhou1709@gmail.com>
2022-06-20 13:46:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*
+- Creazione Array dei post con i seguenti oggetti:
+    a. Id Post con numeroda 1 a n
+    b. Nome Autore
+    c. Foto autore
+    d. Data in formato americano mm-dd-yyyy
+    e. Testo del post
+    f. Immagine (Non tutti i post devono avere un'immagine)
+    g. Numero likes
+- Stampiamo i dati dell'array nell'HTML
+- Al click del tasto mi piace, il colore di quest'ultimo cambia e si incrementano i like relativi. 
